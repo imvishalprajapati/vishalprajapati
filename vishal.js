43197c70cfa94d5dfbefe05f6c68d0a9d9abd6de
Vishal Prajapati <prajapativishalofficial@gmail.com>
2021-05-17 16:55:29 +0530

    update1.0

--- vishal.js
+++ vishal.js
@@ -14,27 +14,17 @@ function mytools(){
 
 
 // greating  area
-function whishes()
-{
-    var din=new Date();
-    var samay=din.getHours();
-    var greet;
-    if(samay<12)
-        {
-            greet="Good morning";
-        }
-    else if(samay>=12 && samay<17)
-        {
-            greet="Good afternoon";
-        }
-    else if(samay>=17 && samay<=20)
-        {
-            greet="Good Evening";
-        }
-    else{
-            greet="Good Night";
-        }
-    document.getElementById('wish').innerHTML =
-        '<b>' + greet + '</b> and welcome to Encodedna.com!';
-    whishes();
-}
+var myDate = new Date();
+var hrs = myDate.getHours();
+
+var greet;
+
+if (hrs < 12)
+    greet = 'Good Morning';
+else if (hrs >= 12 && hrs <= 17)
+    greet = 'Good Afternoon';
+else if (hrs >= 17 && hrs <= 24)
+    greet = 'Good Evening';
+
+document.getElementById('wish').innerHTML =
+    '<b>' + greet + '</b> and welcome to Encodedna.com!';
