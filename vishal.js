163f972816656e160c37cd986925f276307e39dc
Vishal Prajapati <prajapativishalofficial@gmail.com>
2021-05-17 18:13:29 +0530

    ghfhfhdfhfdh

--- vishal.js
+++ vishal.js
@@ -11,19 +11,3 @@ function mytools(){
     }
     mytools();
 }
-var myDate = new Date();
-    var hrs = myDate.getHours();
-
-    var greet;
-
-    if (hrs < 12)
-        greet = 'Good Morning';
-    else if (hrs >= 12 && hrs < 17)
-        greet = 'Good Afternoon';
-    else if (hrs >= 17 && hrs <= 24)
-        greet = 'Good Evening';
-    else{
-        greet= 'Good Night'
-    }
-    document.getElementById('wish').innerHTML =
-         greet + ' and welcome to my Portfolio';
