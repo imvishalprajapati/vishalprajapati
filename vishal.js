a9f0e0003f8255e542659665d5ba32d5af4d2bb5
Vishal Prajapati <prajapativishalofficial@gmail.com>
2021-05-17 19:07:54 +0530

    1.01

--- vishal.js
+++ vishal.js
@@ -1,13 +1,14 @@
-//my tools area
-function mytools(){
-    var a = document.getElementById('km').value;
-    var b = Math.round(a*0.621371);
-    if(a=="" || a<=0)
-    {
-        alert("Please enter a Valid Value")
-    }
-    else{
-    document.getElementById('miles').innerHTML=b+ ' Miles';
-    }
-    mytools();
+var myDate = new Date();
+var hrs = myDate.getHours();
+var greet;
+if (hrs < 12)
+    greet = 'Good Morning';
+else if (hrs >= 12 && hrs < 17)
+    greet = 'Good Afternoon';
+else if (hrs >= 17 && hrs <21)
+    greet = 'Good Evening';
+else{
+    greet= 'Have a Good Night'
 }
+document.getElementById('wish').innerHTML =
+     greet + ' and welcome to my Portfolio';
